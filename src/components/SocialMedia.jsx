5de33bcb89328d4ea47bf63c330caa67aed0b329
Kelvin Rojas <rc19023@ues.edu.sv>
2022-07-10 23:04:02 -0600

    my portfolio with react and sanity

---
+++ src/components/SocialMedia.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { BsTwitter, BsInstagram, BsLinkedin, BsGithub } from 'react-icons/bs';
+
+const SocialMedia = () => (
+  <div className="app__social">
+    <div>
+    <a href='https://twitter.com/21RojasKelvin' target='_blank' rel='noreferrer'>
+      <BsTwitter />
+    </a>
+    </div>
+    <div>
+    <a href='https://www.linkedin.com/in/kelvin-rojas-chilin/' target='_blank' rel='noreferrer'>
+      <BsLinkedin />
+    </a>
+    </div>
+    <div>
+      <a href='https://github.com/DevKelvin21' target='_blank' rel='noreferrer'>
+       <BsGithub />
+      </a>
+    </div>
+    <div>
+      <a href='https://www.instagram.com/k.rojas21/' target='_blank' rel='noreferrer'>
+       <BsInstagram />
+      </a>
+    </div>
+  </div>
+);
+
+export default SocialMedia;
